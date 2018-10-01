ae95b19700ec266bba9f243cb9e98b0a7fbd6a73
PatrickNulty <PatrickNulty@patricks-air>
2018-10-01 12:32:55 +0100

    Test 3

--- Software Engenerring/src/LCATest.java
+++ Software Engenerring/src/LCATest.java
@@ -36,5 +36,35 @@ public class LCATest {
 		assertEquals("LCA of of 3 & 5: ", 1, fiveNodes.findLCA(3,5));
 		assertEquals("LCA of of 4 & 5: ", 2, fiveNodes.findLCA(4,5));
 	}
+	
+	//Test3 for node size equaling 12
+	@Test
+	public void test3(){
+		LCA sixteenNodes = new LCA();
+		sixteenNodes.root = new Node(1);
+		sixteenNodes.root.left = new Node(2);
+		sixteenNodes.root.right = new Node(3);
+		sixteenNodes.root.left.left = new Node(4);
+		sixteenNodes.root.left.right = new Node(5);
+		sixteenNodes.root.right.left = new Node(6);
+		sixteenNodes.root.right.right = new Node(7);
+		sixteenNodes.root.left.left.left = new Node(8);
+		sixteenNodes.root.left.left.right = new Node(9);
+		sixteenNodes.root.left.right.left = new Node(10);
+		sixteenNodes.root.left.right.right = new Node(11);
+		sixteenNodes.root.right.left.left = new Node(12);
+		
+		assertEquals("LCA of of 2 & 3: ", 1, sixteenNodes.findLCA(2,3));
+		assertEquals("LCA of of 2 & 4: ", 2, sixteenNodes.findLCA(2,4));
+		assertEquals("LCA of of 2 & 5: ", 2, sixteenNodes.findLCA(2,5));
+		assertEquals("LCA of of 3 & 4: ", 1, sixteenNodes.findLCA(3,4));
+		assertEquals("LCA of of 3 & 5: ", 1, sixteenNodes.findLCA(3,5));
+		assertEquals("LCA of of 4 & 5: ", 2, sixteenNodes.findLCA(4,5));
+		assertEquals("LCA of of 4 & 7: ", 1, sixteenNodes.findLCA(4,7));
+		assertEquals("LCA of of 8 & 9: ", 4, sixteenNodes.findLCA(8,9));
+		assertEquals("LCA of of 5 & 8: ", 2, sixteenNodes.findLCA(5,8));
+		assertEquals("LCA of of 9 & 10: ", 2, sixteenNodes.findLCA(9,10));
+		assertEquals("LCA of of 11 & 12: ", 1, sixteenNodes.findLCA(11,12));
+	}
 }
  
