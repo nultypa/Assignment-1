90fee688f98e5755580f8c39640fd82757402326
PatrickNulty <PatrickNulty@patricks-air>
2018-10-01 11:55:54 +0100

    test1

--- Software Engenerring/src/LCATest.java
+++ Software Engenerring/src/LCATest.java
@@ -4,16 +4,24 @@ import org.junit.Test;
 
 public class LCATest {
 
-	//Test 0 tests for an empty tree
+	//Test0 tests for an empty tree
 	@Test
 	public void test0() {
 		LCA emptyTree = new LCA();
-		assertEquals("LCA(empty",-1,emptyTree.findLCA(0,0));
+		assertEquals("LCA of an empty tree",-1,emptyTree.findLCA(0,0));
 	}
 	
+	//Test1 for node size equaling 1
 	@Test
 	public void test1(){
+		LCA oneNode = new LCA();
+		oneNode.root = new Node(1);
+		assertEquals("LCA of an tree with one node",-1,oneNode.findLCA(1,0));
+	}
+	
+	//Test2 for node size equaling 5
+	@Test
+	public void test2(){
 		
 	}
-
 }
