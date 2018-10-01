194723d1a4fd1c6f6ba17d7f8578d5f0f08df424
PatrickNulty <PatrickNulty@patricks-air>
2018-10-01 11:35:12 +0100

    Empty test

--- Software Engenerring/src/LCATest.java
+++ Software Engenerring/src/LCATest.java
@@ -4,13 +4,15 @@ import org.junit.Test;
 
 public class LCATest {
 
+	//Test 0 tests for an empty tree
 	@Test
-	public void testForEmptyTree() {
-		
+	public void test0() {
+		LCA emptyTree = new LCA();
+		assertEquals("LCA(empty",-1,emptyTree.findLCA(0,0));
 	}
 	
 	@Test
-	public void testForSize(){
+	public void test1(){
 		
 	}
 
