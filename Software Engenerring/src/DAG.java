2c90f6b27e9c8ac436693c416f6eaf531aa0eb1c
PatrickNulty <PatrickNulty@10.6.74.66>
2018-10-18 14:02:39 +0100

    Dag fix 1

--- Software Engenerring/src/DAG.java
+++ Software Engenerring/src/DAG.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Queue;
 
-class node{
-	int n;
-	Node left;
-	Node right;
-	ArrayList<Node> ansestors;
-	Node(int value)
-	{
-		n = value;
-		left = right;
-		left = null;
-		right = null;
-		ansestors = null;
-	}
-}
 public class DAG {
+
+	private int numE;                 
+	private int numV;           
+	private ArrayList<Integer>[] adj;    
+	private int[] indegreeV;        
+	private boolean marked[];		
+	private boolean cycle;		    
+	private boolean ordV[];	
+	
+	public DAG(int numV){
+		if (numV < 0) throw new IllegalArgumentException("Number of vertices in a Digraph must be nonnegative");
+	    this.numV = numV;
+	    this.numE = 0;
+	    indegreeV = new int[numV];
+	    marked = new boolean[numV];
+	    ordV = new boolean[numV];
+	    adj = (ArrayList<Integer>[]) new ArrayList[numV];
+	    for (int v = 0; v < numV; v++) {
+	    adj[v] = new ArrayList<Integer>(); 
+	    }
+	}
 
 }
