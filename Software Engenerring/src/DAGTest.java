b729baecad2734e32b087551756501bee43fada5
PatrickNulty <PatrickNulty@patricks-air>
2018-10-22 12:58:41 +0100

    To see if DAG works correctly

--- Software Engenerring/src/DAGTest.java
+++ Software Engenerring/src/DAGTest.java
@@ -8,7 +8,19 @@ import static org.junit.Assert.assertEquals;
  	@Test
  	public void test1()
  	{
+ 		DAG test = new DAG(10);
+ 		test.addEdge(1, 2);
+ 		test.addEdge(1, 3);
+ 		test.addEdge(3, 4);
+ 		test.addEdge(4, 5);
+ 		test.addEdge(4, 6);
  		
+ 		assertEquals("", 1, test.indegreeV(4));
+ 		assertEquals("", 2, test.outdegree(4));
+ 		assertEquals("", 5, test.numE());
+ 		assertEquals("", 10, test.numV());
+ 		String ans = "[5, 6]";
+ 		assertEquals("",ans, test.adj(4).toString());
  	}
  	
  	//Test for empty tree
