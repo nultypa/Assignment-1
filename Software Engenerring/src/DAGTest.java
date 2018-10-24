bd16fb26a69dd5aa5e00c74839a5c7f2468dd163
PatrickNulty <PatrickNulty@patricks-air>
2018-10-24 15:39:49 +0100

    Test for no common ancestor

--- Software Engenerring/src/DAGTest.java
+++ Software Engenerring/src/DAGTest.java
@@ -103,5 +103,25 @@ import org.junit.Test;
  		assertEquals("", 2, lca.findLCA(8, 6));
  		assertEquals("", 8, lca.findLCA(9, 8));
 	}
+	@Test
+	//Test for no common ancestor
+ 	public void test8(){
+ 		DAG lca = new DAG(11);
+ 		lca.addEdge(0, 1);
+ 		lca.addEdge(1, 2);
+ 		lca.addEdge(1, 3);
+ 		lca.addEdge(2, 4);
+ 		lca.addEdge(3, 4);
+ 		lca.addEdge(2, 5);
+ 		lca.addEdge(4, 5);
+ 		
+ 		//Common ancestor
+ 		assertEquals("", 1, lca.findLCA(3, 1));
+ 		assertEquals("", 1, lca.findLCA(3, 2));
+ 		
+ 		//No common ancestor
+ 		assertEquals("", -1, lca.findLCA(7, 0));
+ 		assertEquals("", -1, lca.findLCA(11, 10));
+ 	}
 	
  }
